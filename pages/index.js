bb5fb6e64aedae36bcdc72517dce24e5c89a8a37
Aarav <anshuk3917@gmail.com>
2022-11-21 08:32:44 +0530

    Update

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,33 @@
 import { useEffect, useState } from 'react'
 import { ethers } from 'ethers'
-import { client, challenge, authenticate } from '../api'
+import {
+  client, challenge, authenticate, getDefaultProfile,
+  signCreatePostTypedData, lensHub, splitSignature, validateMetadata
+} from '../api'
+import { create } from 'ipfs-http-client'
+import { v4 as uuid } from 'uuid'
+
+const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
+const projectSecret = process.env.NEXT_PUBLIC_PROJECT_SECRET
+const auth = 'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64');
+
+const ipfsClient = create({
+  host: 'ipfs.infura.io',
+  port: 5001,
+  protocol: 'https',
+  headers: {
+      authorization: auth,
+  },
+})
 
 export default function Home() {
-  /* local state variables to hold user's address and access token */
   const [address, setAddress] = useState()
-  const [token, setToken] = useState()
+  const [session, setSession] = useState(null)
+  const [postData, setPostData] = useState('')
+  const [profileId, setProfileId] = useState('')
+  const [handle, setHandle] = useState('')
+  const [token, setToken] = useState('')
   useEffect(() => {
-    /* when the app loads, check to see if the user has already connected their wallet */
     checkConnection()
   }, [])
   async function checkConnection() {
@@ -15,59 +35,136 @@ export default function Home() {
     const accounts = await provider.listAccounts()
     if (accounts.length) {
       setAddress(accounts[0])
+      const response = await client.query({
+        query: getDefaultProfile,
+        variables: { address: accounts[0] }
+      })
+      setProfileId(response.data.defaultProfile.id)
+      setHandle(response.data.defaultProfile.handle)
     }
   }
   async function connect() {
-    /* this allows the user to connect their wallet */
     const account = await window.ethereum.send('eth_requestAccounts')
     if (account.result.length) {
       setAddress(account.result[0])
+      const response = await client.query({
+        query: getDefaultProfile,
+        variables: { address: accounts[0] }
+      })
+      setProfileId(response.data.defaultProfile.id)
+      setHandle(response.data.defaultProfile.handle)
     }
   }
   async function login() {
     try {
-      /* first request the challenge from the API server */
       const challengeInfo = await client.query({
         query: challenge,
-        variables: { address }
+        variables: {
+          address
+        }
       })
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       const signer = provider.getSigner()
-      /* ask the user to sign a message with the challenge info returned from the server */
       const signature = await signer.signMessage(challengeInfo.data.challenge.text)
-      /* authenticate the user */
       const authData = await client.mutate({
         mutation: authenticate,
         variables: {
           address, signature
         }
       })
-      /* if user authentication is successful, you will receive an accessToken and refreshToken */
+
       const { data: { authenticate: { accessToken }}} = authData
-      console.log({ accessToken })
+      localStorage.setItem('lens-auth-token', accessToken)
       setToken(accessToken)
+      setSession(authData.data.authenticate)
     } catch (err) {
       console.log('Error signing in: ', err)
     }
   }
+  async function createPost() {
+    if (!postData) return
+    const ipfsData = await uploadToIPFS()
+    const createPostRequest = {
+      profileId,
+      contentURI: 'ipfs://' + ipfsData.path,
+      collectModule: {
+        freeCollectModule: { followerOnly: true }
+      },
+      referenceModule: {
+        followerOnlyReferenceModule: false
+      },
+    }
+    try {
+      const signedResult = await signCreatePostTypedData(createPostRequest, token)
+      const typedData = signedResult.result.typedData
+      const { v, r, s } = splitSignature(signedResult.signature)
+      const tx = await lensHub.postWithSig({
+        profileId: typedData.value.profileId,
+        contentURI: typedData.value.contentURI,
+        collectModule: typedData.value.collectModule,
+        collectModuleInitData: typedData.value.collectModuleInitData,
+        referenceModule: typedData.value.referenceModule,
+        referenceModuleInitData: typedData.value.referenceModuleInitData,
+        sig: {
+          v,
+          r,
+          s,
+          deadline: typedData.value.deadline,
+        },
+      })
+      console.log('successfully created post: tx hash', tx.hash)
+    } catch (err) {
+      console.log('error posting publication: ', err)
+    }
+  }
+  async function uploadToIPFS() {
+    const metaData = {
+      version: '2.0.0',
+      content: postData,
+      description: postData,
+      name: `Post by @${handle}`,
+      external_url: `https://lenster.xyz/u/${handle}`,
+      metadata_id: uuid(),
+      mainContentFocus: 'TEXT_ONLY',
+      attributes: [],
+      locale: 'en-US',
+    }
 
+    const result = await client.query({
+      query: validateMetadata,
+      variables: {
+        metadatav2: metaData
+      }
+    })
+    console.log('Metadata verification request: ', result)
+      
+    const added = await ipfsClient.add(JSON.stringify(metaData))
+    return added
+  }
+  function onChange(e) {
+    setPostData(e.target.value)
+  }
   return (
     <div>
-      { /* if the user has not yet connected their wallet, show a connect button */ }
       {
         !address && <button onClick={connect}>Connect</button>
       }
-      { /* if the user has connected their wallet but has not yet authenticated, show them a login button */ }
       {
-        address && !token && (
+        address && !session && (
           <div onClick={login}>
             <button>Login</button>
           </div>
         )
       }
-      { /* once the user has authenticated, show them a success message */ }
       {
-        address && token && <h2>Successfully signed in!</h2>
+        address && session && (
+          <div>
+            <textarea
+              onChange={onChange}
+            />
+            <button onClick={createPost}>Create Post</button>
+          </div>
+        )
       }
     </div>
   )
